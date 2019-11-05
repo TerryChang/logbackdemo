e8297e7a561e80e11c3883fc086bfb3e081a3dc5
Terry Chang <furywolf@hanmail.net>
2019-11-05 18:25:40 +0900

    1. 로그 생성시 몇개의 ROW를 생성할 것인지를 상수 변수를 하나 만들어서 이를 이용해서 작업하도록 수정

--- src/main/java/com/terry/logbackdemo/InitializeRunner.java
+++ src/main/java/com/terry/logbackdemo/InitializeRunner.java
@@ -31,6 +31,7 @@ public class InitializeRunner implements CommandLineRunner {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
     private final Logger logger = LoggerFactory.getLogger("testBuyLogger");
+    private final int TOTAL_DATA_CNT = 15;                  // 만들어야 할 총 데이터건수
 
     @Override
     public void run(String... args) throws Exception {
@@ -39,32 +40,44 @@ public class InitializeRunner implements CommandLineRunner {
         jpaversion();
     }
 
+    /**
+     * JPA를 사용해서 로그를 생성하는 함수
+     * @throws Exception
+     */
     private void jpaversion() throws Exception {
         List<User> userList = userRepository.findAllByOrderByIdxAsc();
         List<Product> productList = productRepository.findAllByOrderByIdxAsc();
 
         Random random = new Random();
 
-        List<UserVO> userVOList = userList.stream().map(user -> UserVO.builder().idx(user.getIdx()).name(user.getName()).loginId(user.getLoginId()).build()).collect(Collectors.toList());
-        List<ProductVO> productVOList = productList.stream().map(product -> ProductVO.builder().idx(product.getIdx()).productName(product.getProductName()).productPrice(product.getProductPrice()).build()).collect(Collectors.toList());
+        List<UserVO> userVOList = userList.stream().map(user -> UserVO.builder()
+                                                                    .idx(user.getIdx())
+                                                                    .name(user.getName())
+                                                                    .loginId(user.getLoginId())
+                                                                    .build())
+                                                    .collect(Collectors.toList());
+        List<ProductVO> productVOList = productList.stream().map(product -> ProductVO.builder()
+                                                                                    .idx(product.getIdx())
+                                                                                    .productName(product.getProductName())
+                                                                                    .productPrice(product.getProductPrice())
+                                                                                    .build())
+                                                            .collect(Collectors.toList());
 
         int userVOListSize = userVOList.size();
         int productVOListSize = productVOList.size();
 
-        int productCnt0 = 0;
-
-        for(int i=0; i < 1000000; i++) {
+        for(int i=0; i < TOTAL_DATA_CNT; i++) {
+            // 사용자 리스트를 조회해서 이를 랜덤하게 뽑음
             UserVO userVO = userVOList.get(random.nextInt(userVOListSize));
-
+            // 몇가지 종류의 상품을 살것인지 그 수치를 랜덤한 수치로 뽑는다(1~5 사이로)
             int shoppingItemCount = random.nextInt(5) + 1;
             List<ShoppingItemVO> shoppingItemVOList = new ArrayList<>();
 
             for (int j = 1; j <= shoppingItemCount; j++) {
+                // 임의의 상품을 하나 뽑는다
                 ProductVO productVO = productVOList.get(random.nextInt(productVOListSize));
+                // 상품을 몇개를 살 것인지 그 수치를 1~10 사이의 숫자로 뽑는다
                 int productCnt = random.nextInt(10) + 1;
-
-                if(productCnt == 0) productCnt0++;
-
                 ShoppingItemVO shoppingItemVO = new ShoppingItemVO(productVO, productCnt);
                 shoppingItemVOList.add(shoppingItemVO);
             }
@@ -75,14 +88,13 @@ public class InitializeRunner implements CommandLineRunner {
 
             logger.info(jsonString);
         }
-
-        System.out.println("productCnt0 : " + productCnt0);
-
-
-
     }
 
 
+    /**
+     * DB를 이용하지 않고 가상의 데이터를 만들어서 로그를 구현하는 메소드
+     * @throws Exception
+     */
     private void localDataVersion() throws Exception {
         String [] loginIds = {"microsoft.com", "oracle.com", "adobe.com", "pivotal.com", "jetbrains.com"};
         Long [] productIds = {1L, 2L, 3L, 4L, 5L, 6L};
@@ -92,12 +104,11 @@ public class InitializeRunner implements CommandLineRunner {
 
         List<ProductVO> productVOList = new ArrayList<>();
         for(int i=0; i < productIds.length; i++) {
-            // ProductVO productVO = new ProductVO(productIds[i], prices[i]);
             ProductVO productVO = ProductVO.builder().idx(productIds[i]).productPrice(prices[i]).build();
             productVOList.add(productVO);
         }
 
-        for(int i=0; i < 1000000; i++) {
+        for(int i=0; i < TOTAL_DATA_CNT; i++) {
 
             // 사용자 아이디 random 추출
             String userId = loginIds[random.nextInt(loginIds.length)];
